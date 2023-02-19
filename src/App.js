b3ad9a8cc12f84b262ee123008c4fb43117e3fdd
Ausha123 <aushasandeepa569@gmail.com>
2023-02-19 19:55:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import FavouritesPage from "./pages/Favourite";
 import NewMeetupPage from "./pages/NewMeetup";
 import { Routes } from "react-router-dom";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favourites" element={<FavouritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
