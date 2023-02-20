d72c93d61a1672dee044b415056eccb7130f70cc
Ausha123 <aushasandeepa569@gmail.com>
2023-02-20 10:15:16 +0530

    add useeffect-hook

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
   function onAddMeetupHandler(meetupData) {
     fetch(
       "https://react-getting-started-abbb3-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +13,9 @@ function NewMeetupPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      navigate("/", { replace: true });
+    });
   }
   return (
     <section>
